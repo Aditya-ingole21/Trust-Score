af6e6ded80ee6f102c0ea9f1b4fc772ddfba9819
Aditya-ingole21 <aditayingole113@gmail.com>
2025-11-24 19:34:28 +0530

    solved some security issue

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -18,7 +18,7 @@ address constant BTC_USD_FEED  = 0x51A6D0a79BBf21633098C30cb7aD24f1E19074e4;
 
     function run() public {
         // uint256 key = vm.envUint("DEPLOYER_PRIVATE_KEY");
-        uint256 key = 0x5eb5539b43913fe06afc3f6a4acc5650543bd4b013cfca9ad560c082857503a0;
+     
 
         vm.startBroadcast(key);
 
